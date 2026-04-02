2e0da80edc40351cfa3f38845788c85aa209d7ae
AlBars <103575531+AleksandrBars@users.noreply.github.com>
2026-04-02 19:33:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,16 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// 👇 Замените 'НАЗВАНИЕ-РЕПОЗИТОРИЯ' на реальное имя вашего репозитория
-const REPO_NAME = 'НАЗВАНИЕ-РЕПОЗИТОРИЯ';
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],  // убрал viteSingleFile – он не нужен
-  base: `/${REPO_NAME}/`,            // <- ЭТО САМОЕ ВАЖНОЕ
+  plugins: [react(), tailwindcss()],
+  base: '/shift-schedule-test/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: 'dist',                  // папка, куда Vite соберёт сайт
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
